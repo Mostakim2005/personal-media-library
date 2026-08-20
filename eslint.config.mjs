108685742c5eb8dfe1431ea63b60bffdcf7d4134
Mostakim2005 <160144078+Mostakim2005@users.noreply.github.com>
2026-08-20 08:41:41 +0600

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,14 @@ import tseslint from 'typescript-eslint';
 import obsidian from 'eslint-plugin-obsidianmd';
 
 export default tseslint.config(
-  { ignores: ['main.js', 'scripts/**/*.mjs', 'tests/**/*.mjs'] },
+  {
+    ignores: [
+      'main.js',
+      'esbuild.config.mjs',
+      'scripts/**/*.mjs',
+      'tests/**/*.mjs',
+    ],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   obsidian.configs.recommended,
